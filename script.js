ab35e587654b0fcb72640b1351dbf5ec13fb0f01
Yousef ayman <yousefayman2000500@gmail.com>
2024-08-05 16:54:29 +0300

    showing the numbers on the UI

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ const toggleDarkThemeWithEnter = (event)=>{
 themeToggle.addEventListener("click",toggleDarkTheme)
 themeToggle.addEventListener("keypress",toggleDarkThemeWithEnter)
 
+
+// Logic for calculator
+let currentNumber = "";
+let storedNumber = "";
+let operation ="";
+
+const result = document.querySelector(".calc__result")
+const keyElements = document.querySelectorAll("[data-type]")
+
+const updateUI =(value)=>{
+    result.innerText = value ? value :"0";
+}
+
+const numberClickHandler = (value)=>{
+        if(value === 0 && !currentNumber )return;
+        if(value === "." && currentNumber.includes("."))return;
+        currentNumber += value
+        updateUI(currentNumber)
+}
+
+const keyElementHandler = (el)=>{
+    el.addEventListener("click",()=>{
+        el.dataset.type === "number"&& numberClickHandler(el.dataset.value);
+    })
+}
+
+keyElements.forEach(keyElementHandler)
+
